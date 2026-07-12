d9b50758774dad99a9e0de8e984b55b773bc7264
Yaroslav B. [Homelab PC] <debian1500@gmail.com>
2026-07-12 22:08:45 +0500

    add tvicons & russia1.svg

--- make_m3u8.py
+++ make_m3u8.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
 
-print("M3U/M3U8 IPTV Playlist Editor")
+print("M3U8 IPTV Playlist Editor")
 
+
+playlist_name = input("Введите имя плейлиста (можно создать новый): ")
 stream_link = input("Введите ссылку на поток: ")
 stream_name = input("Введите имя канала/потока: ")
 stream_icon = input("Введите ссылку на иконку: ")
-playlist_name = input("Введите имя плейлиста (должен лежать в одной папке со скриптом: ")
 
 
-with open (playlist_name, 'a', encoding='utf-8') as playlist_file:
-	print("Here's a playlist: ", playlist_file)
-	
+# Для всех четырёх переменных:
+# Добавить проверку пустого ввода
+# Добавить проверку наличия в файле
+
+
+# Проверяем наличие расширения, если нету - сами добавляем
+if not playlist_name.endswith('.m3u8'):
+    playlist_name += '.m3u8'
+
+# Строка для добавления
+entry = f'#EXTINF:-1 tvg-name="{stream_name}" tvg-logo="{stream_icon}" tvg-country="RU" group-title="Russia",{stream_name}\n{stream_link}\n'
+
+
+with open (playlist_name, 'a', encoding='utf-8') as f:
+   f.write(entry)	
+print("Канал добавлен")
